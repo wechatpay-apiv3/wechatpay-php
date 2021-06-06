66220aa192891e533f3bdb82fa4259dba392e654
James ZHNAG <thenorthmemory@qq.com>
2021-06-06 15:21:30 +0800

    Builder.php & BuilderTrait.php for wechatpay-apiv3/wechatpay-guzzle-middleware#51

---
+++ src/Builder.php
@@ -0,0 +1,135 @@
+<?php
+namespace WechatPay\GuzzleMiddleware;
+
+use WechatPay\GuzzleMiddleware\ClientDecorator;
+
+/**
+ * Chainable the client for sending HTTP requests.
+ */
+class Builder
+{
+    /**
+     * Building & decorate the chainable `\GuzzleHttp\Client`
+     *
+     * @example
+     * $wxpay = Builder::factory([]);
+     * $res = $wxpay['v3/merchantService/complaintsV2']->get(['debug' => true]);
+     * $res = $wxpay['v3/merchant-service/complaint-notifications']->get(['debug' => true]);
+     * $wxpay->v3->merchantService->ComplaintNotifications->postAsync([])->wait();
+     * $wxpay->v3->certificates->getAsync()->then()->otherwise()->wait();
+     *
+     * @param array $config - configuration
+     * @param string|int $config[mchid] - The merchant ID
+     * @param string $config[serial] - The serial number of the merchant certificate
+     * @param OpenSSLAsymmetricKey|OpenSSLCertificate|resource|array|string $config[privateKey] - The merchant private key.
+     * @param array $config[certs] - The WeChatPay platform serial and certificate(s), `[serial => certificate]` pair
+     *
+     * @return \ArrayIterator - The chainable client
+     */
+    public static function factory(array $config = [])
+    {
+        return new class([], new ClientDecorator($config)) extends \ArrayIterator {
+            use BuilderTrait;
+
+            /**
+             * Compose the chainable `ClientDecorator` instance, most starter with the tree root point
+             */
+            public function __construct(array $input = [], ?ClientDecorator $instance = null) {
+                parent::__construct($input, self::STD_PROP_LIST | self::ARRAY_AS_PROPS);
+
+                $this->setDriver($instance);
+            }
+
+            /**
+             * @var ClientDecorator $driver - The `ClientDecorator` instance
+             */
+            protected $driver;
+
+            /**
+             * `$driver` setter
+             * @param ClientDecorator $instance - The `ClientDecorator` instance
+             *
+             * @return self
+             */
+            public function setDriver(ClientDecorator &$instance)
+            {
+                $this->driver = $instance;
+
+                return $this;
+            }
+
+            /**
+             * `$driver` getter
+             *
+             * @return ClientDecorator - The `ClientDecorator` instance
+             */
+            public function getDriver(): ClientDecorator
+            {
+                return $this->driver;
+            }
+
+            /**
+             * Normalize the `$thing` by the rules: `PascalCase` -> `camelCase`
+             *                                    & `camelCase` -> `camel-case`
+             *                                    & `_dynamic_` -> `{dynamic}`
+             *
+             * @param string thing - The string waiting for normalization
+             *
+             * @return string
+             */
+            protected function normalize(string $thing = ''): string
+            {
+                return \preg_replace('/^_(.*)_$/', '{\1}', \preg_replace_callback('/[A-Z]/', function($piece) {
+                    return '-' . \strtolower($piece[0]);
+                }, \preg_replace_callback('/^[A-Z]/', function($piece) {
+                    return \strtolower($piece);
+                }, $thing)));
+            }
+
+            /**
+             * URI pathname
+             *
+             * @param string [$seperator = '/'] - The URI seperator
+             *
+             * @return string - The URI string
+             */
+            protected function pathname(string $seperator = '/'): string
+            {
+                return \implode($seperator, $this->simplized());
+            }
+
+            /**
+             * Only retrieve a copy array of the URI entities
+             *
+             * @return array - The URI entities' array
+             */
+            protected function simplized(): array
+            {
+                return \array_filter($this->getArrayCopy(), function($v) { return !($v instanceof self); });
+            }
+
+            /**
+             * Chainable the given `$key` with the `ClientDecorator` instance
+             *
+             * @param string|int $key - The key
+             *
+             * @return self
+             */
+            public function offsetGet($key)
+            {
+                if (!$this->offsetExists($key)) {
+                  $index = $this->simplized();
+                  \array_push($index, $this->normalize($key));
+                  $this->offsetSet($key, new self($index, $this->getDriver()));
+                }
+
+                return parent::offsetGet($key);
+            }
+        };
+    }
+
+    private function __construct()
+    {
+        // cannot be instantiated
+    }
+}
---
+++ src/BuilderTrait.php
@@ -0,0 +1,111 @@
+<?php
+namespace WechatPay\GuzzleMiddleware;
+
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Chainable points the client interface for sending HTTP requests.
+ */
+trait BuilderTrait
+{
+    /**
+     * Create and send an HTTP GET request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function get(array $options = []): ResponseInterface
+    {
+        return $this->getDriver()->request('GET', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an HTTP PUT request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function put(array $options = []): ResponseInterface
+    {
+        return $this->getDriver()->request('PUT', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an HTTP POST request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function post(array $options = []): ResponseInterface
+    {
+        return $this->getDriver()->request('POST', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an HTTP PATCH request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function patch(array $options = []): ResponseInterface
+    {
+        return $this->getDriver()->request('PATCH', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an HTTP DELETE request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function delete(array $options = []): ResponseInterface
+    {
+        return $this->getDriver()->request('DELETE', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP GET request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function getAsync(array $options = []): PromiseInterface
+    {
+        return $this->getDriver()->requestAsync('GET', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP PUT request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function putAsync(array $options = []): PromiseInterface
+    {
+        return $this->getDriver()->requestAsync('PUT', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP POST request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function postAsync(array $options = []): PromiseInterface
+    {
+        return $this->getDriver()->requestAsync('POST', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP PATCH request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function patchAsync(array $options = []): PromiseInterface
+    {
+        return $this->getDriver()->requestAsync('PATCH', $this->pathname(), $options);
+    }
+
+    /**
+     * Create and send an asynchronous HTTP DELETE request.
+     *
+     * @param array $options Request options to apply.
+     */
+    public function deleteAsync(array $options = []): PromiseInterface
+    {
+        return $this->getDriver()->requestAsync('DELETE', $this->pathname(), $options);
+    }
+}
