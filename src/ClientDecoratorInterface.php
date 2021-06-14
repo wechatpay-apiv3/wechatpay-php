992e3c284b595064a772420b8e0aa7664934c8d3
James ZHNAG <thenorthmemory@qq.com>
2021-06-14 15:54:05 +0800

    `request[Async]` methods signature

--- src/ClientDecoratorInterface.php
+++ src/ClientDecoratorInterface.php
@@ -1,8 +1,12 @@
 <?php
+
 namespace WechatPay\GuzzleMiddleware;
 
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Message\ResponseInterface;
+
 /**
- * Decorate the `\GuzzleHttp\Client` interface
+ * Decorate the `GuzzleHttp\Client` interface
  */
 interface ClientDecoratorInterface
 {
@@ -10,4 +14,26 @@ interface ClientDecoratorInterface
      * @var string - The WechatPayMiddleware version
      */
     const VERSION = '1.0.0';
+
+    /**
+     * Request the remote `$pathname` by a HTTP `$method` verb
+     *
+     * @param string $pathname - The pathname string.
+     * @param string $method - The method string.
+     * @param array $options - The options.
+     *
+     * @return ResponseInterface - The `Psr\Http\Message\ResponseInterface` instance
+     */
+    public function request(string $method, string $pathname, array $options = []): ResponseInterface;
+
+    /**
+     * Async request the remote `$pathname` by a HTTP `$method` verb
+     *
+     * @param string $pathname - The pathname string.
+     * @param string $method - The method string.
+     * @param array $options - The options.
+     *
+     * @return PromiseInterface - The `GuzzleHttp\Promise\PromiseInterface` instance
+     */
+    public function requestAsync(string $method, string $pathname, array $options = []): PromiseInterface;
 }
