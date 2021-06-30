5465a5108e77087a0a1a1955b260045a4d171bfe
James ZHNAG <thenorthmemory@qq.com>
2021-06-30 16:36:29 +0800

    [WIP] RsaTest

---
+++ tests/Crypto/RsaTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace WeChatPay\Tests\Crypto;
+
+use function openssl_pkey_new;
+use function openssl_pkey_get_details;
+
+use const OPENSSL_KEYTYPE_RSA;
+
+use WeChatPay\Crypto\Rsa;
+use PHPUnit\Framework\TestCase;
+
+class RsaTest extends TestCase
+{
+    /**
+     * @return array<string,array{string,string|resource|mixed,resource|mixed}>
+     */
+    public function keysProvider(): array
+    {
+        $privateKey = openssl_pkey_new([
+            'digest_alg' => 'sha256',
+            'private_key_bits' => 2048,
+            'private_key_type' => OPENSSL_KEYTYPE_RSA
+        ]);
+        ['key' => $publicKey] = openssl_pkey_get_details($privateKey);
+
+        return [
+            'plaintext, publicKey and privateKey' => ['hello wechatpay', $publicKey, $privateKey]
+        ];
+    }
+
+    /**
+     * @dataProvider keysProvider
+     */
+    public function testEncrypt(string $plaintext, $publicKey): void
+    {
+        $ciphertext = Rsa::encrypt($plaintext, $publicKey);
+        self::assertIsString($ciphertext);
+        self::assertNotEquals($plaintext, $ciphertext);
+    }
+
+    /**
+     * @dataProvider keysProvider
+     */
+    public function testDecrypt(string $plaintext, $publicKey, $privateKey): void
+    {
+        $ciphertext = Rsa::encrypt($plaintext, $publicKey);
+        self::assertIsString($ciphertext);
+        self::assertNotEquals($plaintext, $ciphertext);
+        $mytext = Rsa::decrypt($ciphertext, $privateKey);
+        self::assertIsString($mytext);
+        self::assertEquals($plaintext, $mytext);
+    }
+}
