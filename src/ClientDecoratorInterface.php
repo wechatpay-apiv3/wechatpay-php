a321951fd26702d93e89b592c13169a019957871
James ZHNAG <thenorthmemory@qq.com>
2021-07-21 08:58:12 +0800

    notes of the `dev`, `beta` and `stable` versions

--- src/ClientDecoratorInterface.php
+++ src/ClientDecoratorInterface.php
@@ -14,7 +14,7 @@ interface ClientDecoratorInterface
     /**
      * @var string - This library version
      */
-    const VERSION = '1.0.5';
+    const VERSION = '1.0.6';
 
     /**
      * @var string - The HTTP transfer `xml` based protocol
