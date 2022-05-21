b63a6b4a6e6a1b0381100c73fbac44c4f81363d5
James ZHNAG <thenorthmemory@qq.com>
2022-05-21 18:28:59 +0800

    bump to v1.4.5

--- src/ClientDecoratorInterface.php
+++ src/ClientDecoratorInterface.php
@@ -14,7 +14,7 @@ interface ClientDecoratorInterface
     /**
      * @var string - This library version
      */
-    public const VERSION = '1.4.4';
+    public const VERSION = '1.4.5';
 
     /**
      * @var string - The HTTP transfer `xml` based protocol
